7621b292ccfe7a2d16030db752aa04c9609df681
Mike Sheldon <msheldon@nwtools4ever.com>
2025-11-07 12:55:26 -0900

    Update Epic Systems.ps1

--- Epic Systems.ps1
+++ Epic Systems.ps1
@@ -7,13 +7,20 @@ $Log_MaskableKeys = @(
     "proxy_password"
 )
 
+$Global:UserCacheTime = Get-Date
+$Global:User = [System.Collections.ArrayList]@()
+
 $Properties = @{
     User = @(
         @{ name = 'ID';              type = 'string';   objectfields = $null;             options = @('default','key') },
-        @{ name = 'Name';            type = 'string'; objectfields = $null;             options = @('default') },
-        @{ name = 'ExternalID';      type = 'string'; objectfields = $null;             options = @('default') },
-        @{ name = 'EnRolRank';       type = 'string'; objectfields = $null;             options = @('default') },
-        @{ name = 'Status';          type = 'string';   objectfields = @('personId');     options = @('default') }
+        @{ name = 'Name';            type = 'string';   objectfields = $null;             options = @('default') },
+        @{ name = 'ExternalID';      type = 'string';   objectfields = $null;             options = @('default') },
+        @{ name = 'EnRolRank';       type = 'string';   objectfields = $null;             options = @('default') },
+        @{ name = 'Status';          type = 'string';   objectfields = $null;             options = @('default') }
+    )
+    UserGroup = @(
+        @{ name = 'UserID';              type = 'string';   objectfields = $null;             options = @('default') },
+        @{ name = 'Group';              type = 'string';   objectfields = $null;             options = @('default') }
     )
 }
 
@@ -185,19 +192,22 @@ function Idm-UsersRead {
             Get-ClassMetaData -SystemParams $SystemParams -Class $Class
             return 
         }
-        # Refresh cache if needed
-        $properties = Get-ClassProperties -Class $Class -IncludeHidden $true
 
-        # Initial empty SearchStateContext
-        $searchStateContext = @{
-            Identifier   = ''
-            ResumeInfo   = ''
-            CriteriaHash = ''
-        }
-        $i = 0;
-        do {
-            # Build SearchStateContext XML
-            $searchStateXml = @"
+        # Refresh cache if needed
+        if ($script:User.Count -eq 0 -or ((Get-Date) - $script:UserCacheTime).TotalMinutes -gt 5) {
+            $Global:User.Clear()
+            $properties = Get-ClassProperties -Class $Class -IncludeHidden $true
+
+            # Initial empty SearchStateContext
+            $searchStateContext = @{
+                Identifier   = ''
+                ResumeInfo   = ''
+                CriteriaHash = ''
+            }
+            $i = 0;
+            do {
+                # Build SearchStateContext XML
+                $searchStateXml = @"
 <SearchStateContext>
     <Identifier>$($searchStateContext.Identifier)</Identifier>
     <ResumeInfo>$($searchStateContext.ResumeInfo)</ResumeInfo>
@@ -205,19 +215,19 @@ function Idm-UsersRead {
 </SearchStateContext>
 "@
 
-            $splat = @{ 
-                    SystemParams = $system_params
-                    Class = $Class
-                    Uri = '/interconnect-amcurprd-personnel-username/httplistener.ashx'
-                    Action = 'urn:epicsystems-com:Core.2008-04.Services.GetUserRecords'
-                    ActionResponse = 'urn:epicsystems-com:Core.2008-04.Services.GetUserRecords'
-                    Body = ''
-                    LogMessage = if($i -gt 0) { "Result Count: [$($i)]" }
-            }
+                $splat = @{ 
+                        SystemParams = $system_params
+                        Class = $Class
+                        Uri = '/interconnect-amcurprd-personnel-username/httplistener.ashx'
+                        Action = 'urn:epicsystems-com:Core.2008-04.Services.GetUserRecords'
+                        ActionResponse = 'urn:epicsystems-com:Core.2008-04.Services.GetUserRecords'
+                        Body = ''
+                        LogMessage = if($i -gt 0) { "Result Count: [$($i)]" }
+                }
 
 
-            # Update SOAP body
-            $splat.Body = @"
+                # Update SOAP body
+                $splat.Body = @"
 <GetUserRecords xmlns="urn:epicsystems-com:Core.2008-04.Services">
     <ConnectionFunctionalType/>
     <SearchCriteria>
@@ -228,50 +238,112 @@ function Idm-UsersRead {
 </GetUserRecords>
 "@
 
-            # Send request
-            $response = Execute-SOAPRequest @splat
+                # Send request
+                $response = Execute-SOAPRequest @splat
 
-            # Namespace manager
-            $nsMgr = New-Object System.Xml.XmlNamespaceManager($response.NameTable)
-            $nsMgr.AddNamespace("soap", "http://schemas.xmlsoap.org/soap/envelope/")
-            $nsMgr.AddNamespace("core", "urn:epicsystems-com:Core.2008-04.Services")
+                # Namespace manager
+                $nsMgr = New-Object System.Xml.XmlNamespaceManager($response.NameTable)
+                $nsMgr.AddNamespace("soap", "http://schemas.xmlsoap.org/soap/envelope/")
+                $nsMgr.AddNamespace("core", "urn:epicsystems-com:Core.2008-04.Services")
 
-            # Extract and output records
-            $records = $response.SelectNodes("//core:ResultRecord", $nsMgr)
+                # Extract and output records
+                $records = $response.SelectNodes("//core:ResultRecord", $nsMgr)
 
-            foreach ($rowItem in $records) {
-                $row = New-Object -TypeName PSObject -Property ([ordered]@{ })
+                foreach ($rowItem in $records) {
+                    $row = New-Object -TypeName PSObject -Property ([ordered]@{ })
 
-                foreach ($prop in $properties.GetEnumerator()) {
-                    $row | Add-Member -NotePropertyName $prop -NotePropertyValue ""
+                    foreach ($prop in $properties.GetEnumerator()) {
+                        $row | Add-Member -NotePropertyName $prop -NotePropertyValue ""
+                    }
+
+                    foreach ($prop in $rowItem.PSObject.Properties) {
+                        if($prop.Name -eq 'AdditionalFields') {
+                            $row.Status = ($prop.Value.Field | Where-Object { $_.Title -eq "Status" }).Value
+                            continue
+                        }
+                        
+                        if ($properties.Contains($prop.Name)) {
+                            $row.($prop.Name) = $prop.Value
+                        }
+                    }
+                    $i++
+                    [void]$Global:User.Add($row)  # Output immediately
                 }
 
-                foreach ($prop in $rowItem.PSObject.Properties) {
-                    if($prop.Name -eq 'AdditionalFields') {
-                        $row.Status = ($prop.Value.Field | Where-Object { $_.Title -eq "Status" }).Value
-                        continue
-                    }
-                    
-                    if ($properties.Contains($prop.Name)) {
-                        $row.($prop.Name) = $prop.Value
-                    }
+                # Update SearchStateContext for next page
+                $sscNode = $response.SelectSingleNode("//core:SearchStateContext", $nsMgr)
+                $searchStateContext = @{
+                    Identifier   = $sscNode.Identifier
+                    ResumeInfo   = $sscNode.ResumeInfo
+                    CriteriaHash = $sscNode.CriteriaHash
                 }
-                $i++
-                $row  # Output immediately
+
+                # Continue if any values are non-empty
+                $hasMore = ($searchStateContext.Identifier -or $searchStateContext.ResumeInfo -or $searchStateContext.CriteriaHash)
+            } while ($hasMore)
+        }
+
+        $Global:User
+}
+
+function Idm-UserGroupsRead {
+    param (
+        # Mode
+        [switch] $GetMeta,    
+        # Parameters
+        [string] $SystemParams,
+        [string] $FunctionParams
+
+    )
+        $system_params   = ConvertFrom-Json2 $SystemParams
+        $function_params = ConvertFrom-Json2 $FunctionParams
+        $Class = 'UserGroup'
+        
+        if ($GetMeta) {
+            Get-ClassMetaData -SystemParams $SystemParams -Class $Class
+            return
+        }
+
+        # Refresh cache if needed
+        if ($Global:Users.Count -eq 0 -or ((Get-Date) - $Global:UsersCacheTime).TotalMinutes -gt 5) {
+            Idm-UsersRead -SystemParams $SystemParams -FunctionParams $FunctionParams | Out-Null
+        }
+
+        # Precompute property template
+        $properties = $Global:Properties.$Class | Where-Object { ('hidden' -notin $_.options ) }
+        $propertiesHT = @{}; $Global:Properties.$Class | ForEach-Object { $propertiesHT[$_.name] = $_ }
+
+        $template = [ordered]@{}
+        foreach ($prop in $properties.Name) {
+            $template[$prop] = $null
+        }
+        
+        # TODO: Needs to be converted to be multi-threaded
+        $result = foreach ($item in $Global:User) {
+            $splat = @{
+                SystemParams = $system_params             
+                Uri = '/interconnect-amcurprd-username/api/epic/2016/Security/PersonnelManagement/ViewUserGroups/Personnel/User/Groups/View'
+                Body = (@{ "UserID" = @{ "ID" = $item.ID; "Type"= "EXTERNAL"}}) | ConvertTo-Json
+                Class = $Class
+                LogMessage = "[$($item.ID)]"
+            }
+            try {
+                $response = Execute-Request @splat
+            } catch {
+                Log warning "Retrieve User Groups [$($item.ID)] - $_"
+                continue
             }
 
-            # Update SearchStateContext for next page
-            $sscNode = $response.SelectSingleNode("//core:SearchStateContext", $nsMgr)
-            $searchStateContext = @{
-                Identifier   = $sscNode.Identifier
-                ResumeInfo   = $sscNode.ResumeInfo
-                CriteriaHash = $sscNode.CriteriaHash
+            foreach($membershipRow in $response.UserGroups) {
+                $row = New-Object -TypeName PSObject -Property $template
+
+                $row.UserID = $item.Id
+                $row.Group = $membershipRow
+
+                $row
             }
-
-            # Continue if any values are non-empty
-            $hasMore = ($searchStateContext.Identifier -or $searchStateContext.ResumeInfo -or $searchStateContext.CriteriaHash)
-        } while ($hasMore)
-
+        }
+        $result
 }
 
 #
@@ -412,6 +484,91 @@ public class TrustAllCertsPolicy : ICertificatePolicy {
             }
         }
     } while ($true)
+}
+
+function Execute-Request {
+    param (
+        [hashtable] $SystemParams,
+        [string] $Uri,
+        [string] $Body,
+        [string] $Class,
+        [string] $LogMessage
+    )
+    # Setup  authorization token
+    $pair = "emp`$$($SystemParams.username):$($SystemParams.password)"
+    $encodedCreds = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($pair))
+
+    # Build request parameters
+    $splat = @{
+        Headers = @{
+            Authorization = "Basic $($encodedCreds)"
+            'Epic-Client-ID' = $SystemParams.clientId
+            Accept = "application/json"
+            'Content-Type' = "application/json"
+        }
+        Method = "POST"
+        Uri = ("https://{0}{1}" -f $SystemParams.hostname, $uri)
+        Body = $Body
+    }
+
+    # Configure proxy if enabled
+    if ($SystemParams.use_proxy) {
+        # Avoid redefining TrustAllCertsPolicy class unnecessarily
+        if (-not ([System.Net.ServicePointManager]::CertificatePolicy -is [TrustAllCertsPolicy])) {
+            Add-Type -TypeDefinition @"
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+public class TrustAllCertsPolicy : ICertificatePolicy {
+    public bool CheckValidationResult(
+        ServicePoint srvPoint, X509Certificate certificate,
+        WebRequest request, int certificateProblem) {
+        return true;
+    }
+}
+"@ -ErrorAction Stop
+            [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+        }
+
+        $splat["Proxy"] = $SystemParams.proxy_address
+
+        if ($SystemParams.use_proxy_credentials) {
+            $splat["ProxyCredential"] = New-Object System.Management.Automation.PSCredential (
+                $SystemParams.proxy_username,
+                (ConvertTo-SecureString $SystemParams.proxy_password -AsPlainText -Force)
+            )
+        }
+    }
+
+    $attempt = 0
+    $retryDelay = $SystemParams.retryDelay
+
+    do {
+        try {
+            $attemptSuffix = if ($attempt -gt 0) { " (Attempt $($attempt + 1))" } else { "" }
+            
+            $baseMessage = "$($splat.Method) Call: $($splat.Uri)$($attemptSuffix) [$($Class)] $($LogMessage)"
+            Log verbose $baseMessage
+
+            $response = Invoke-RestMethod @splat -ErrorAction Stop
+            break
+        }
+        catch {
+            if($errorBreak) { throw}
+            
+            $statusCode = $_.Exception.Response.StatusCode.value__
+            if ($statusCode -eq 429 -and $attempt -lt $SystemParams.nr_of_retries) {
+                $attempt++
+                Log warning "Received $statusCode. Retrying in $retryDelay seconds..."
+                Start-Sleep -Seconds $retryDelay
+                $retryDelay *= 2 # Exponential backoff
+            }
+            else {
+                throw "Failed request: $_"
+            }
+        }
+    } while ($true)
+
+    return $response
 }
 
 function Get-ClassMetaData {
